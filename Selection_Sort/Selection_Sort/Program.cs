f1afc9d28864ca39a1fee64683b2bc5e072b5b59
19Mukhtar98 <mukhtarpashazade164@gmail.com>
2023-10-24 12:11:19 +0400

    selection sort

--- Selection_Sort/Selection_Sort/Program.cs
+++ Selection_Sort/Selection_Sort/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] arr = new int[5] { 4, 7, 2, 1, 8 };
+int min = arr[0];
+for (int i = 0; i < arr.Length-1; i++)
+{
+    int LowNumberIndex = i;
+    for (int j = i+1; j < arr.Length; j++)
+    {
+        if (arr[j] < arr[LowNumberIndex])
+        {
+            LowNumberIndex = j;
+        }
+    }
+    if(LowNumberIndex!=i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[LowNumberIndex];
+        arr[LowNumberIndex] = temp;
+    }
+}
+foreach (var value in arr)
+{
+    Console.WriteLine(value);
+}
