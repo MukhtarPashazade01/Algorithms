836814d2fb4d58f8eec959e557c2a2d30b1eecec
19Mukhtar98 <mukhtarpashazade164@gmail.com>
2023-10-24 11:01:10 +0400

    first_test

---
+++ Bubble_Sort/Bubble_Sort/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = new int[5] { 5, 3, 2, 7, 1 };
+int bigValue = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[i] < arr[j])
+        {
+            bigValue = arr[j];
+            arr[j] = arr[i];
+            arr[i] = bigValue;
+
+           
+        }
+    }
+}
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
