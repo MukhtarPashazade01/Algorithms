6e85c47e5d3ed453cddb3558b9047ae88dbb9e05
19Mukhtar98 <mukhtarpashazade164@gmail.com>
2023-10-28 11:15:12 +0400

    bubble sort

--- Bubble_Sort/Bubble_Sort/Program.cs
+++ Bubble_Sort/Bubble_Sort/Program.cs
@@ -1,17 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int[] arr = new int[5] { 5, 3, 2, 7, 1 };
-int bigValue = 0;
+int temp = 0;
 for (int i = 0; i < arr.Length; i++)
 {
-    for (int j = 0; j < arr.Length; j++)
+    for (int j = 0; j < arr.Length - i - 1; j++)
     {
-        if (arr[i] < arr[j])
+        if (arr[j] > arr[j + 1])
         {
-            bigValue = arr[j];
-            arr[j] = arr[i];
-            arr[i] = bigValue;
+            temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
 
-           
+
         }
     }
 }
